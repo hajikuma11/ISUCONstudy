dc45aa57062989309b899a4386c03f60aa2ec59a
hajikuma11 <hajimekuma11@gmail.com>
2020-07-20 01:15:11 +0900

    hello go

--- webapp/main.go
+++ webapp/main.go
@@ -8,8 +8,10 @@ func main() {
 	engine := gin.Default()
 	engine.LoadHTMLGlob("templates/*.html")
 
+	data := "Go."
+
 	engine.GET("/", func(cntxt *gin.Context) {
-		cntxt.HTML(200, "index.html", gin.H{})
+		cntxt.HTML(200, "index.html", gin.H{"data": data})
 	})
 
 	engine.Run()
